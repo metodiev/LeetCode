0bfd4b460450d679a695710b1a8151d08176709b
y509199 <miroslav.metodiev@softwareag.com>
2021-06-15 09:19:27 +0300

    Add Two Sum solution from Leet Code: https://leetcode.com/problems/two-sum/solution/

---
+++ TestSum.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 1999 - 2011 my-Channels Ltd
+ * Copyright (c) 2012 - 2021 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.
+ *
+ * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG.
+ */
+
+package com.pcbsys.nirvana.client.clusterClientConnection;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestSum {
+
+  public static void main(String[] args) {
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+    twoSum(nums, target);
+    twoSumMap(nums, target);
+    onePassHashTable(nums, target);
+  }
+
+  public static int[] twoSum(int[] nums, int target) {
+    ArrayList list = new ArrayList();
+    if (nums.length >= 2) {
+      for (int i = 0; i < nums.length; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+          if (nums[i] + nums[j] == target) {
+            list.add(i);
+            list.add(j);
+          }
+        }
+      }
+    }
+    int[] myArray = new int[list.size()];
+
+    for (int i = 0; i < list.size(); i++) {
+      myArray[i] = (int) list.get(i);
+    }
+    return myArray;
+  }
+
+  public  static int[]  twoSumMap(int[] nums, int target){
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length ; i++) {
+      map.put(nums[i], i);
+    }
+
+    for (int i = 0; i < nums.length ; i++) {
+      int compement = target - nums[i];
+      if (map.containsKey(compement) && map.get(compement) != i){
+        return new int[] {i, map.get(compement)};
+      }
+    }
+    return nums;
+  }
+
+  public static int[] onePassHashTable(int[] nums, int target){
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length ; i++) {
+      int complement = target - nums[i];
+      if (map.containsKey(complement)){
+        return new int[] {map.get(complement), i};
+      }
+      map.put(nums[i], i);
+    }
+
+    return nums;
+  }
+
+}
