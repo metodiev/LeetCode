b0f1bc891c09b2816fe229f4ea97c8a69e949184
metodiev <miroslav.l.metodiev@gmail.com>
2021-06-15 09:20:50 +0300

    Update TestSum.java

--- TestSum.java
+++ TestSum.java
@@ -1,9 +1,4 @@
-/*
- * Copyright (c) 1999 - 2011 my-Channels Ltd
- * Copyright (c) 2012 - 2021 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.
- *
- * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG.
- */
+
 
 package com.pcbsys.nirvana.client.clusterClientConnection;
 
